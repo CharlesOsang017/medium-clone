9cf482d8fea169ec6a95e499278b74b370a77ace
CharlesOsang017 <charlesosango02@gmail.com>
2024-01-27 16:01:20 +0300

    add demo header

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,18 @@
-export function App() {
-  
+import { Route, Routes } from "react-router-dom";
+import Demo from "./components/demo/Demo";
+import Home from "./components/home/Home";
+import HomeHeader from "./components/home/HomeHeader";
+import DemoHeader from "./components/demo/DemoHeader";
 
+export function App() {
+  const auth = false;
   return (
-    <div className="h1 underline">Hellow world</div>
+    <>
+    {auth ? <HomeHeader /> : <DemoHeader />}
+    <Routes>
+      <Route path='/demo' element={<Demo />}/>
+      <Route path='/' element={<Home />}/>
+    </Routes>
+    </>
   )
 }
---
+++ src/components/demo/Demo.jsx
@@ -0,0 +1,46 @@
+
+
+const Demo = () => {
+  return (
+    <>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    <div>Demo</div>
+    </>
+  )
+}
+
+export default Demo
